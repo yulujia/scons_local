ddcbd59002865cf4f74ae6543d40381ba6607d6b
Yulu Jia <yulu.jia@intel.com>
2019-03-27 20:53:36 +0000

    add build scripts for just me
    Signed-off-by: Yulu Jia <yulu.jia@intel.com>

---
+++ utils/sl-compile-ompi.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# TARGET_PREFIX
+#    specify where to install  executables, libraries, and header files
+#    - works for all components suppported by scons local
+#    - each component will create its own sub directory in that path.
+#        e.g. TARGET_PATH=mypath
+#        mercury will create:
+#            mypath
+#            |-- mercury
+#                |-- include
+#                |-- lib
+#                `-- share
+# PREBUILT_PREFIX
+#    specify where to find installed components. This is to be paired with
+#    TARGET_PREFIX.
+#    - works for all components
+#    - It expects each component to be in its own sub directory.
+#        e.g. PREBUILT_PREFIX=mypath, scons local will assume mercury like:
+#            mypath
+#            |-- mercury
+#                |-- include
+#                |-- lib
+#                `-- share
+# PREFIX
+#    puts headers, libs from all components in the same tree. A component won't
+#    create its own sub directory.
+#    e.g. PREFIX=mypath stuff from mercury and cart will be in the tree:
+#                mypath
+#                |-- include
+#                |-- lib
+#                `-- share
+
+OPTS=(COMPILER=gcc \
+      MERCURY_SRC=/home/yulujia/codes/mercury-github \
+      TARGET_PREFIX=/home/yulujia/codes/prebuilt-with-scons-local-boro-36 \
+      --build-config=/home/yulujia/codes/scons-local-build-boro-36/utils/build.config \
+      --config=force \
+      --build-deps=yes \
+      VERBOSE=1 \
+      -j8 \
+)
+scons "${OPTS[@]}" install REQUIRES=ompi | tee compile-ompi.log
